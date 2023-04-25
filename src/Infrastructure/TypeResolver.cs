42f31edf211de93321c363c219962772fd19c068
Ira Rainey <irarainey@microsoft.com>
2023-04-25 13:51:26 +0100

    Updated nullabilty of type resolver.

--- src/Infrastructure/TypeResolver.cs
+++ src/Infrastructure/TypeResolver.cs
@@ -11,7 +11,7 @@ namespace Beeching.Infrastructure
             _provider = provider ?? throw new ArgumentNullException(nameof(provider));
         }
 
-        public object Resolve(Type type)
+        public object? Resolve(Type? type)
         {
             if (type == null)
             {
